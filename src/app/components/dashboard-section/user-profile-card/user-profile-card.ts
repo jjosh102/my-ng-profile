126dd833ad46732829bc52df8ee4e66227bf80e1
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-09-23 10:05:09 +0800

    Edit bio

--- src/app/components/dashboard-section/user-profile-card/user-profile-card.ts
+++ src/app/components/dashboard-section/user-profile-card/user-profile-card.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class UserProfileCard {
   user = {
     name: 'Josh J Piluden',
-    bio: '.NET developer by day, tech tinkerer by night—always curious, always building, and loving every bit of the journey!',
+    bio: '.NET developer who enjoys tinkering with tech in my free time—always learning and building along the way.',
     githubUrl: 'https://github.com/jjosh102',
     email: 'joshuajpiluden@gmail.com',
     avatarUrl: 'https://avatars.githubusercontent.com/u/50490077?v=4'
